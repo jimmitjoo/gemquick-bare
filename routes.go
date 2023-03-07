af3987a13dcc3706132960bdd59fba65eaf835b3
Jimmie Johansson <jimmitjoo@gmail.com>
2023-03-07 14:10:08 +0100

    Remove unused things

--- routes.go
+++ routes.go
@@ -10,13 +10,13 @@ import (
 )
 
 func (route *application) routes() *chi.Mux {
+
 	// middleware must come before any routes
-	route.use(route.Middleware.CheckRemember)
 
 	// add routes here
 	route.get("/", route.Handlers.Home)
 
-	// static routes
+	// static routes for images and other static files
 	fileServer := http.FileServer(http.Dir("./public"))
 	route.App.Routes.Handle("/public/*", http.StripPrefix("/public", fileServer))
 
